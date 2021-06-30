8c055c9753a3db613d47b2990b97f6ceecd01a3a
Simon Bartels <bartels@di.ku.dk>
2021-06-30 21:45:42 +0200

    bug fixes for simon's code

--- make_plot_brute_force.py
+++ make_plot_brute_force.py
@@ -1,14 +1,18 @@
 import mlflow
 import numpy as np
+from gpflow.kernels import SquaredExponential
+from mlflow.entities import ViewType
 
+from algorithms.gp_on_real_space import GPonRealSpace
 from algorithms.one_hot_gp import GPOneHotSequenceSpace
-from util.mlflow.constants import DATASET, METHOD, MSE
+from util.mlflow.constants import DATASET, METHOD, MSE, REPRESENTATION
 from util.mlflow.convenience_functions import find_experiments_by_tags
 from visualization.plot_metric_for_dataset import plot_metric_for_dataset
 
 # gathers all our results and saves them into a numpy array
 dataset = "1FQG"
 metric = MSE
+representations = [None, "transformer"]
 algorithm_name_list = []
 result_list = []
 last_result_length = None
@@ -17,18 +21,20 @@ last_result_length = None
 # Jacob's code
 
 # Simon's code
-simons_algos = [GPOneHotSequenceSpace(alphabet_size=0)]
-for a in simons_algos:
-    exps = find_experiments_by_tags({DATASET: dataset, METHOD: a.get_name()})
-    assert(len(exps) == 1)
-    algorithm_name_list.append(a.get_name())
-    runs = mlflow.search_runs(experiment_ids=[exps[0].experiment_id])
-    results = []
-    for id in runs['run_id'].to_list():
-        results.append(mlflow.tracking.MlflowClient().get_metric_history(id, metric))
-    if len(result_list) > 0:
-        assert(len(results) == len(result_list[-1]))
-    result_list.append(results)
+simons_algos = {#None: [GPOneHotSequenceSpace(alphabet_size=0), GPOneHotSequenceSpace(alphabet_size=0, kernel=SquaredExponential())],
+                "transformer": [GPonRealSpace(), GPonRealSpace(kernel=SquaredExponential())]}
+for repr in simons_algos.keys():
+    for a in simons_algos[repr]:
+        exps = find_experiments_by_tags({DATASET: dataset, METHOD: a.get_name(), REPRESENTATION: repr})
+        assert(len(exps) == 1)
+        algorithm_name_list.append(a.get_name())
+        runs = mlflow.search_runs(experiment_ids=[exps[0].experiment_id], run_view_type=ViewType.ACTIVE_ONLY)
+        results = []
+        for id in runs['run_id'].to_list():
+            results.append(mlflow.tracking.MlflowClient().get_metric_history(id, metric))
+        if len(result_list) > 0:
+            assert(len(results) == len(result_list[-1]))
+        result_list.append(results)
 
 
 # Richard's code
