f97eb0ca282d20218c1cf535650b3b74dc27f635
Simon Bartels <bartels@di.ku.dk>
2021-08-03 15:34:53 +0200

    changed resultslist to dictionaries

--- make_plot_brute_force.py
+++ make_plot_brute_force.py
@@ -13,24 +13,19 @@ from visualization.plot_metric_for_dataset import plot_metric_for_dataset
 datasets = ["CALM", "BRCA"]
 metric = MSE
 representations = [VAE, TRANSFORMER]
-algorithm_name_list = []
 results_dict = {}
 last_result_length = None
 
 
-# Jacob's code
-
-# Simon's code
 simons_algos = {VAE: [GPOneHotSequenceSpace(alphabet_size=0), GPOneHotSequenceSpace(alphabet_size=0, kernel=SquaredExponential())],
                 TRANSFORMER: [GPonRealSpace(), GPonRealSpace(kernel=SquaredExponential())]}
 for dataset in datasets:
     result_dict = {}
     for repr in simons_algos.keys():
+        result_dict[repr] = {}
         for a in simons_algos[repr]:
             exps = find_experiments_by_tags({DATASET: dataset, METHOD: a.get_name(), REPRESENTATION: repr})
             assert(len(exps) == 1)
-            if a.get_name() not in algorithm_name_list:
-                algorithm_name_list.append(a.get_name())
             runs = mlflow.search_runs(experiment_ids=[exps[0].experiment_id], run_view_type=ViewType.ACTIVE_ONLY)
             results = []
             for id in runs['run_id'].to_list():
@@ -38,12 +33,11 @@ for dataset in datasets:
                     results.append(r.value)
             # if len(result_list) > 0:
             #     assert(len(results) == len(result_list[-1]))
-            result_dict[a.get_name()] = results
+            result_dict[repr][a.get_name()] = results
     results_dict[dataset] = result_dict
 
-# Richard's code
 print(results_dict)
 # then calls #plot_metric_for_dataset
-plot_metric_for_dataset(datasets=datasets, metric_values=results_dict, reps=algorithm_name_list, cvtype="cv split")
+plot_metric_for_dataset(datasets=datasets, metric_values=results_dict, reps=[], cvtype="cv split")
 
 
