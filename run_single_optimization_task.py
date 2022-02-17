3300aea4aff51fa108216b00a1da5cdd054fdc18
Richard Michael <richard.michael.1995@gmail.com>
2022-02-17 20:29:50 +0100

    added predict_f to optimization task

--- run_single_optimization_task.py
+++ run_single_optimization_task.py
@@ -1,11 +1,15 @@
 import numpy as np
 import mlflow
+import warnings
+import tensorflow as tf
+from tqdm import tqdm
 from scipy.stats import norm as normal
-
 from algorithms.abstract_algorithm import AbstractAlgorithm
-from data.load_dataset import load_dataset
+from gpflow.kernels import SquaredExponential
+from algorithms.gp_on_real_space import GPonRealSpace
+from data.load_dataset import load_dataset, get_alphabet
 from util.mlflow.constants import DATASET, METHOD, REPRESENTATION, TRANSFORMER, SEED, OPTIMIZATION, EXPERIMENT_TYPE, \
-    OBSERVED_Y
+    OBSERVED_Y, VAE, ONE_HOT
 from util.mlflow.convenience_functions import find_experiments_by_tags, make_experiment_name_from_tags
 
 
@@ -25,8 +29,12 @@ def run_single_optimization_task(dataset: str, method: AbstractAlgorithm, seed: 
     X = X[p, :]
     Y = Y[p, :]
 
-    tags = {EXPERIMENT_TYPE: OPTIMIZATION, DATASET: dataset, METHOD: method.get_name(), REPRESENTATION: representation,
+    tags = {EXPERIMENT_TYPE: OPTIMIZATION, 
+            DATASET: dataset, 
+            METHOD: method.get_name(), 
+            REPRESENTATION: representation,
             SEED: str(seed)}
+
     exp = find_experiments_by_tags(tags)
     if len(exp) == 0:
         experiment_name = make_experiment_name_from_tags(tags)
@@ -42,36 +50,32 @@ def run_single_optimization_task(dataset: str, method: AbstractAlgorithm, seed: 
     next = 0
     selected_X = []
     mlflow.start_run()
-    for i in range(1, max_iterations+1):
+    for i in tqdm(range(1, max_iterations+1)):
         selected_X.append(next)
         # the .sum() is a hack to get a float value--mlflow complains about numpy arrays
         mlflow.log_metric(OBSERVED_Y, np.squeeze(Y[next, :]).sum(), step=i)
         X_observed = X[selected_X, :]
         Y_observed = Y[selected_X, :]
         method.train(X_observed, Y_observed)
-
         remaining_X = np.setdiff1d(np.arange(X.shape[0]), selected_X, assume_unique=True)
+
         candidates = X[remaining_X, :]
-        mu, unc = method.predict(candidates)
+        mu, unc = method.predict_f(candidates)
         assert(mu.shape[1] == 1 == unc.shape[1])
         assert(mu.shape[0] == unc.shape[0] == candidates.shape[0])
         # we are minimizing
         eta = np.min(Y_observed, axis=0)
         scoring = _expected_improvement(mu, unc, eta)
         # the acquisition function we maximize
-        best_candidate = np.argmax(scoring)
+        best_candidate = np.argmax(scoring, axis=0)[0]
         next = remaining_X[best_candidate]
     mlflow.end_run()
 
 
 if __name__ == "__main__":
     dataset = "BRCA"
-    from util.mlflow.constants import VAE
     representation = VAE
-    from data.load_dataset import get_alphabet
     alphabet = get_alphabet(dataset)
-    from gpflow.kernels import SquaredExponential
-    from algorithms.gp_on_real_space import GPonRealSpace
 
     run_single_optimization_task(dataset=dataset, representation=representation,
                                  method=GPonRealSpace(kernel=SquaredExponential()), seed=0)
