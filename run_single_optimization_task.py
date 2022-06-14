corrected scaling uncertainty in regression and optimization

run_single_optimization_task.py
@@ -59,35 +59,42 @@ def run_single_optimization_task(dataset: str, method_key: str, seed: int, repre
         mlflow.log_metric(OBSERVED_Y, np.squeeze(Y[next, :]).sum(), step=i)
         X_observed = X[selected_X, :]
         Y_observed = Y[selected_X, :]
+        # if enough data, scale observations
+        if len(selected_X) > 1:
+            mean_y, std_y, Y_observed = scale_observations(Y_observed) # outputs scaled_y as Y_observed
         method.train(X_observed, Y_observed)
         remaining_X = np.setdiff1d(np.arange(X.shape[0]), selected_X, assume_unique=True)
         remaining_Y = Y[remaining_X, :]
         candidates = X[remaining_X, :]
-        mu, unc = method.predict_f(candidates)
-        assert(mu.shape[1] == 1 == unc.shape[1])
-        assert(mu.shape[0] == unc.shape[0] == candidates.shape[0])
+        _mu, _unc = method.predict_f(candidates)
+        assert(_mu.shape[1] == 1 == _unc.shape[1])
+        assert(_mu.shape[0] == _unc.shape[0] == candidates.shape[0])
         # we are minimizing
         eta = np.min(Y_observed, axis=0)
-        scoring = _expected_improvement(mu, unc, eta)
+        scoring = _expected_improvement(_mu, _unc, eta)
         # the acquisition function we maximize
         best_candidate = np.argmax(scoring, axis=0)[0]
         next = remaining_X[best_candidate]
 
-        Sharpness = np.std(unc, ddof=1)/np.mean(unc)
+        Sharpness = np.std(_unc, ddof=1)/np.mean(_unc)
         mlflow.log_metric('Sharpness', Sharpness, step=i)
         EI_y_corr = stats.spearmanr(scoring,remaining_Y)[0]
         #print_summary(method.gp)
         mlflow.log_metric('EI_y_corr', EI_y_corr, step=i)
         if i in log_interval:
-            _log_optimization_metrics_to_mlflow(method=method, remaining_Y=remaining_Y, mean_y=np.mean(Y_observed), 
-                                                mu=mu, unc=unc, step=i)
+            _log_optimization_metrics_to_mlflow(method=method, remaining_Y=remaining_Y, mean_y=mean_y, std_y=std_y,
+                                                _mu=_mu, _unc=_unc, step=i)
     mlflow.end_run()
 
 
-def _log_optimization_metrics_to_mlflow(method, remaining_Y, mean_y, mu, unc, step) -> None:
+def _log_optimization_metrics_to_mlflow(method, remaining_Y, mean_y, std_y, _mu, _unc, step) -> None:
     """
     Compute metrics and write to registered mlflow experiment.
     """
+    # undo scaling
+    mu = _mu*std_y + mean_y
+    unc = _unc * std_y
+    # compute metrics
     baseline = np.mean(np.square(remaining_Y - np.repeat(mean_y, len(remaining_Y)).reshape(-1,1)))
     err2 = np.square(remaining_Y - mu)
     mse = np.mean(err2)/baseline
@@ -95,7 +102,6 @@ def _log_optimization_metrics_to_mlflow(method, remaining_Y, mean_y, mu, unc, st
     mse_var = np.var(err2)
     mll = np.mean(err2 / unc / 2 + np.log(2 * np.pi * unc) / 2)
     r = spearmanr(remaining_Y, mu)[0]  # we do not care about the p-value
-
     mlflow.log_metric(MSE, mse, step=step)
     mlflow.log_metric(MedSE, medse, step=step)
     mlflow.log_metric(SEVar, mse_var, step=step)

run_single_regression_task.py
@@ -19,7 +19,7 @@ from util.numpy_one_hot import numpy_one_hot_2dmat
 from util.log_uncertainty import prep_for_logdict
 from util.mlflow.constants import AUGMENTATION, DATASET, METHOD, MSE, MedSE, SEVar, MLL, SPEARMAN_RHO, REPRESENTATION, SPLIT, ONE_HOT, VAE_DENSITY
 from util.mlflow.constants import GP_L_VAR, GP_LEN, GP_VAR, GP_MU, OPT_SUCCESS
-from util.mlflow.constants import NON_LINEAR, LINEAR, GP_K_PRIOR, GP_D_PRIOR
+from util.mlflow.constants import NON_LINEAR, LINEAR, GP_K_PRIOR, GP_D_PRIOR, MEAN_Y, STD_Y
 from util.preprocess import scale_observations
 from gpflow.utilities import print_summary
 from gpflow import kernels
@@ -88,12 +88,13 @@ def run_single_regression_task(dataset: str, representation: str, method_key: st
             method.init_length = np.max(np.abs(np.subtract(X_train[0], X_train[1]))) # if reduced on lower dim this value is too small
         method.train(X_train, scaled_y)
         try:
-            _mu, unc = method.predict_f(X_test)
+            _mu, _unc = method.predict_f(X_test)
         except tf.errors.InvalidArgumentError as _:
             warnings.warn(f"Experiment: {dataset}, rep: {representation} in d={dim} not stable, prediction failed!")
-            _mu, unc = np.full(Y_test.shape, np.nan), np.full(Y_test.shape, np.nan)
+            _mu, _unc = np.full(Y_test.shape, np.nan), np.full(Y_test.shape, np.nan)
         # undo scaling
         mu = _mu*std_y + mean_y
+        unc = _unc * std_y
         assert(mu.shape[1] == 1 == unc.shape[1])
         assert(mu.shape[0] == unc.shape[0] == len(test_indices[split]))
         # record mean and median smse and nll and spearman correlation
@@ -106,14 +107,19 @@ def run_single_regression_task(dataset: str, representation: str, method_key: st
         mse_var = np.var(err2)
         mll = np.mean(err2 / unc / 2 + np.log(2 * np.pi * unc) / 2)
         r = spearmanr(Y_test, mu)[0]  # we do not care about the p-value
+
         if split == 1:
-            plot_mid_training(X_test, Y_test, mu, unc, method_key)
+            if "GP" in method.get_name():
+                unc += method.gp.likelihood.variance.numpy()
+            plot_mid_training(X_test, Y_test, mu, unc, method)
 
         mlflow.log_metric(MSE, mse, step=split)
         mlflow.log_metric(MedSE, medse, step=split)
         mlflow.log_metric(SEVar, mse_var, step=split)
         mlflow.log_metric(MLL, mll, step=split)
         mlflow.log_metric(SPEARMAN_RHO, r, step=split)
+        mlflow.log_metric(MEAN_Y, mean_y, step=split)  # record scaling information 
+        mlflow.log_metric(STD_Y, std_y, step=split)
         if "GP" in method.get_name():
             mlflow.log_metric(GP_VAR, float(method.gp.kernel.variance.numpy()), step=split)
             mlflow.log_metric(GP_L_VAR, float(method.gp.likelihood.variance.numpy()), step=split)