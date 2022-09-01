3df76c46550e6d4f7052dcaac9ecc442b2238b44
Richard Michael <r.michael@posteo.net>
2022-09-01 15:16:01 +0200

    account for splits in parsing

--- util/postprocess.py
+++ util/postprocess.py
@@ -37,6 +37,8 @@ def parse_baseline_mutation_observations(results_dict: dict, metric: callable=np
         train_mutations = _dict.get(cv_split).get("train_mutation")
         training_observations = np.array(_dict.get(cv_split).get("train_trues"))
         flat_train_mutations = [("".join(_m), i) for i, _m in enumerate(train_mutations)]
+        split_observations = []
+        split_additive_values = []
         for test_mutation, test_val in zip(_dict.get(cv_split).get("test_mutation"), _dict.get(cv_split).get("trues")):
             idx = []
             # BASE CASE: go over all mutations components (true for single, double, ... in test)
@@ -44,8 +46,8 @@ def parse_baseline_mutation_observations(results_dict: dict, metric: callable=np
                 for m, i in flat_train_mutations:
                     if mutation==m:
                         idx.append(i)
-            true_observations.append(test_val)
-            additive_training_values.append(metric(training_observations[idx]))
+            split_observations.append(test_val)
+            split_additive_values.append(metric(training_observations[idx]))
             if len(test_mutation) > 2: # case of k variants look at combination 
                 for mutation in combinations(test_mutation, len(test_mutation)-1): # TODO extend to k-mutations by combinations int is range
                     remainder = np.setdiff1d(test_mutation, mutation)
@@ -53,7 +55,9 @@ def parse_baseline_mutation_observations(results_dict: dict, metric: callable=np
                     remainder_idx = [i for m, i in flat_train_mutations if m==remainder]
                     idx = source_mutations_idx + remainder_idx # concatenate lists of indices
                     # for each combination one observation
-                    true_observations.append(test_val)
-                    additive_training_values.append(metric(training_observations[idx]))
-    assert len(additive_training_values) == len(true_observations)       
+                    split_observations.append(test_val)
+                    split_additive_values.append(metric(training_observations[idx]))
+        assert len(split_additive_values) == len(split_observations)
+        true_observations.append(split_observations)
+        additive_training_values.append(split_additive_values)       
     return np.array(additive_training_values), np.array(true_observations)
