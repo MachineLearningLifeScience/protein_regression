c0515de0a683e7244d19c7de7e26446e6ad6c868
Richard Michael <r.michael@posteo.net>
2022-09-01 16:16:43 +0200

    added biosplitter test 2->2; 3->3 CV case

--- test/test_splitting/test_splitter_general.py
+++ test/test_splitting/test_splitter_general.py
@@ -13,3 +13,11 @@ def test_train_test_split_exclusive(splitter: callable):
     train, _, test = splitter("1FQG").split(X)
     for _train, _test in zip(train, test):
         assert all([t not in _test for t in _train])
+
+
+@pytest.mark.parametrize("mutations", [(2,2), (3,3)])
+def test_biosplitter_intradomain(mutations):
+    X, _ = load_dataset("TOXI")
+    train, _, test = BioSplitter("TOXI", mutations[0], mutations[1]).split(X)
+    for _train, _test in zip(train, test):
+        assert all([t not in _test for t in _train])
