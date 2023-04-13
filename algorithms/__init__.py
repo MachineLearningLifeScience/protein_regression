8a1194896173f000486a45278826f4ada5c8b6b8
Richard Michael <r.michael@posteo.net>
2023-04-13 10:57:27 +0200

    added number of parameters per model as constant in module

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -5,3 +5,12 @@ from algorithms.one_hot_gp import GPOneHotSequenceSpace
 from algorithms.random_forest import RandomForest
 from algorithms.uncertain_rf import Uncertain_RandomForestRegressor, UncertainRandomForest
 from algorithms.gmm_regression import GMMRegression
+
+
+NUM_TRAINABLE_PARAMETERS = {"kNN": 1, 
+                            "uncertainRF": 100,
+                            "RF": 100, # NOTE: this is default configuration, if optimization=True, has be queried from results
+                            "GPlinear": 2, # theta = {sigma, data_sigma}
+                            "GPsquexp": 3, # theta = {sigma, l, data_sigma}
+                            "GPmatern": 3,
+                            }
