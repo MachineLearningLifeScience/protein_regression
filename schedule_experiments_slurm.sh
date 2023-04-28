ddc5d5a96d7eb5e5fd5f4d6949df01ad803c387f
pcq275 <pcq275@hendrixgpu13fl.unicph.domain>
2023-04-28 13:45:50 +0200

    updated conda loading and module loads for run script

--- schedule_experiments_slurm.sh
+++ schedule_experiments_slurm.sh
@@ -5,6 +5,13 @@
 #SBATCH --array=1-10%3
 #SBATCH --time=12-12:00:00
 
+module load cuda/11.8
+module load cudnn/8.6.0
+
+OUTPUT_LOG=/home/pcq275/protein_regression/slurm_experiment_run_out.log
+
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate protein_regression
 
 CONFIG=/home/pcq275/protein_regression/slurm_experiment_config.txt
@@ -14,5 +21,5 @@ representation=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $3}' 
 protocol=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $4}' ${CONFIG})
 method=$(awk -v ArrayID=${SLURM_ARRAY_TASK_ID} '$1==ArrayID {print $5}' ${CONFIG})
 
-echo "python /home/pcq275/protein_regression/run_experiments.py -d ${dataset} -r ${representation} -p ${protocol} -m ${method}"
-#python /home/pcq275/protein_regression/run_experiments.py ${SLURM_ARRAY_TASK_ID}
+echo "python /home/pcq275/protein_regression/run_experiments.py -d ${dataset} -r ${representation} -p ${protocol} -m ${method}" >> ${OUTPUT_LOG}
+python /home/pcq275/protein_regression/run_experiments.py ${SLURM_ARRAY_TASK_ID} >> ${OUTPUT_LOG}
