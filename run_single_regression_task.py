b2e5b123964b0623dbbec3e11c0c36a945eb619f
Richard Michael <r.michael@posteo.net>
2023-04-28 10:58:41 +0200

    wrap execution in tqdm

--- run_single_regression_task.py
+++ run_single_regression_task.py
@@ -189,7 +189,7 @@ def run_single_regression_task(dataset: str, representation: str, method_key: st
 
     # run protocol for experiment in parallel
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(_run_regression_at_index_function_wrapper, range(0, len(train_indices)))
+        tqdm(executor.map(_run_regression_at_index_function_wrapper, range(0, len(train_indices))), total=len(range(0, len(train_indices))))
     # Parallel(n_jobs=-1)(delayed(_run_regression_at_split)(X=X, Y=Y, train_indices=train_indices, test_indices=test_indices, split=idx, method=method, dim=dim, dim_reduction=dim_reduction, dataset=dataset, representation=representation, protocol=protocol, augmentation=augmentation) 
     #         for idx in tqdm(range(0, len(train_indices))))
 
