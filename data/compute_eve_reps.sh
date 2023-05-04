5d1845f926808030f87d29b7b72699b7d9bfe0c1
pcq275 <pcq275@hendrixgpu13fl.unicph.domain>
2023-05-04 12:27:11 +0200

    update EVE representation computation

--- data/compute_eve_reps.sh
+++ data/compute_eve_reps.sh
@@ -1,11 +1,13 @@
-conda activate eve_env
+#!/bin/bash
 
-/home/pcq275/unmount_erda.sh
-/home/pcq275/mount_erda.sh
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
+conda activate protein_env
 
-MNT_DIR=/home/pcq275/mnt
+#/home/pcq275/unmount_erda.sh
+#/home/pcq275/mount_erda.sh
 
-MNT_DIR=/home/pcq275/mnt
+MNT_DIR=/home/pcq275/mnt_pr
 MSA_DIR=${MNT_DIR}/alignments
 MSA_LIST=${MNT_DIR}/eve_mappings.csv
 VAE_CHECKPOINT=${MNT_DIR}/eve_checkpoint
@@ -18,28 +20,30 @@ output_evol_indices_location=${MNT_DIR}/eve_results/
 num_samples_compute_evol_indices=2000
 batch_size=2048
 
-for idx in $(seq 0 6); do
-# BLAT is idx=1
-echo ${idx}
-python /home/pcq275/EVE/compute_evol_indices.py \
-    --MSA_data_folder ${MSA_DIR} \
-    --MSA_list ${MSA_LIST} \
-    --protein_index ${idx} \
-    --MSA_weights_location ${VAE_CHECKPOINT} \
-    --VAE_checkpoint_location ${VAE_CHECKPOINT} \
-    --model_name_suffix ${SUFFIX} \
-    --model_parameters_location ${MODEL_PARAMETERS} \
-    --computation_mode ${computation_mode} \
-    --all_singles_mutations_folder ${all_singles_mutations_folder} \
-    --output_evol_indices_location ${output_evol_indices_location} \
-    --num_samples_compute_evol_indices ${num_samples_compute_evol_indices} \
-    --batch_size ${batch_size}
-done
+#for idx in $(seq 0 6); do
+## BLAT is idx=1
+#echo ${idx}
+#python /home/pcq275/EVE/compute_evol_indices.py \
+#    --MSA_data_folder ${MSA_DIR} \
+#    --MSA_list ${MSA_LIST} \
+#    --protein_index ${idx} \
+#    --MSA_weights_location ${VAE_CHECKPOINT} \
+#    --VAE_checkpoint_location ${VAE_CHECKPOINT} \
+#    --model_name_suffix ${SUFFIX} \
+#    --model_parameters_location ${MODEL_PARAMETERS} \
+#    --computation_mode ${computation_mode} \
+#    --all_singles_mutations_folder ${all_singles_mutations_folder} \
+#    --output_evol_indices_location ${output_evol_indices_location} \
+#    --num_samples_compute_evol_indices ${num_samples_compute_evol_indices} \
+#    --batch_size ${batch_size}
+#done
 
 # !WARN! SPECIAL CASE: BRCA1 , TOXI
 # FOR BRCA all_singles list != the experimental mutations:
-idx=4 # idx=7 for TOXI
+idx=7 # idx=4 BRCA1; idx=7 for TOXI
 computation_mode='input_mutations_list'
+## MUTATIONS: 	BRCA => BRCA1_HUMAN_Findaly_2018.csv
+##		TOXI => TOXI.csv
 mutations_location=/home/pcq275/EVE/data/mutations # -> BRCA.csv == BRCA1_HUMAN_Findlay_2018.csv
 python /home/pcq275/EVE/compute_evol_indices.py \
     --MSA_data_folder ${MSA_DIR} \
