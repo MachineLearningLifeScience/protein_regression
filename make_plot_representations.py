0f3d6f6409870148555afa6f08cf5653c36b39bf
Richard Michael <r.michael@posteo.net>
2023-05-08 12:14:01 +0200

    set seed for UMAP visualization
    for reproducability

--- make_plot_representations.py
+++ make_plot_representations.py
@@ -9,17 +9,17 @@ def plot_reduced_representations(dataset: str, representation: str, augmentation
     # TODO: UMAP reduce X
     # TODO: figure of 2D X colored by assay values
     X, Y = load_dataset(dataset, representation=representation, augmentation=augmentation)
-    reducer = UMAP()
+    reducer = UMAP(transform_seed=42)
     emb = reducer.fit_transform(X)
     plt.scatter(emb[:, 0], emb[:, 1], c=Y, cmap="magma", s=30, alpha=0.75, edgecolors="black")
     plt.title(f"2D UMAP {dataset} {representation}")
     plt.savefig(f"./results/figures/representations/{dataset}_{representation}_UMAP.png")
     plt.savefig(f"./results/figures/representations/{dataset}_{representation}_UMAP.pdf")
-    plt.show()
+    #plt.show()
 
 
 if __name__ == "__main__":
-    datasets = ["MTH3", "TIMB", "CALM", "1FQG", "UBQT", "BRCA"]
+    datasets = ["MTH3", "TIMB", "CALM", "1FQG", "UBQT", "BRCA"] # ["MTH3", "TIMB", "CALM", "1FQG", "UBQT", "BRCA"]
     representations = [EVE_DENSITY, TRANSFORMER, ONE_HOT, ESM, EVE]
     for dataset in datasets:
         for representation in representations:
