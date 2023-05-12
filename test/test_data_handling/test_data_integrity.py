de88a17725b609b05221f5159deb589da13cc1c2
Richard Michael <r.michael@posteo.net>
2023-05-12 16:10:30 +0200

    added test for TOXI data consistent at loading

--- test/test_data_handling/test_data_integrity.py
+++ test/test_data_handling/test_data_integrity.py
@@ -1,5 +1,6 @@
 import pytest
 from data import load_dataset
+from data.load_dataset import load_one_hot
 from data.load_dataset import __load_df
 import numpy as np
 from util.mlflow.constants import ONE_HOT, EVE, ESM, TRANSFORMER
@@ -22,3 +23,10 @@ def test_loaded_observations_against_data_DF(dataset):
     _, y_df = __load_df(df_name, "seqs")
     _, y_load = load_dataset(dataset, representation=ONE_HOT)
     np.testing.assert_equal(-y_df, y_load)
+
+
+def test_toxi_sequences_equal_OH_rep():
+    toxi_sequences, toxi_obs_seq = load_one_hot("TOXI")
+    toxi_data, toxi_obs = load_dataset("TOXI")
+    assert toxi_sequences.shape[0] == toxi_data.shape[0]
+    np.testing.assert_equal(toxi_obs_seq, -toxi_obs) # NOTE: loading data inverts observations.
