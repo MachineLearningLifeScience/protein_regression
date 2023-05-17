7f572b66f49f145b3f1bda1d05468bdf921347ab
Peter Mørch Groth <petermoercgroth@gmail.com>
2023-05-17 09:34:05 +0200

    Changed cached_results to True

--- make_plot_uncertainties.py
+++ make_plot_uncertainties.py
@@ -16,7 +16,7 @@ from util.mlflow.constants import NO_AUGMENT, ROSETTA, TRANSFORMER, VAE, VAE_RAN
 from util.mlflow.constants import SPLIT, ONE_HOT, ESM, NONSENSE, KNN_name, VAE_DENSITY
 from visualization.plot_metric_for_uncertainties import plot_uncertainty_eval, plot_uncertainty_eval_across_dimensions
 
-datasets = ["1FQG", "UBQT"] # 1FQG, UBQT, CALM
+datasets = ["1FQG"] #, "UBQT"] # 1FQG, UBQT, CALM
 # datasets = ["TOXI"]
 # train_test_splitter = BioSplitter(datasets[0], 3, 4) # BlockPostionSplitter # PositionSplitter # RandomSplitter # BlockPostionSplitter # BioSplitter
 
@@ -31,19 +31,20 @@ algos = [UncertainRandomForest().get_name(),
         GPonRealSpace(kernel_factory= lambda: SquaredExponential()).get_name(),]
 d = None # 2, 10, 100, 1000, None
 dim_reduction = LINEAR # LINEAR # NON_LINEAR
+cached_results = True
 
 
 if __name__ == "__main__":
-        # ### MAKE UNCERTAIN PLOTS RANDOMSPLITTER
+        ### MAKE UNCERTAIN PLOTS RANDOMSPLITTER
         plot_uncertainty_eval(datasets=datasets, reps=reps,
-                          algos=algos, train_test_splitter=RandomSplitter(datasets[0]), 
-                          augmentations = augmentations, number_quantiles=number_quantiles, optimize=True, 
-                          d=d, dim_reduction=None)
+                          algos=algos, train_test_splitter=RandomSplitter(datasets[0]),
+                          augmentations = augmentations, number_quantiles=number_quantiles, optimize=True,
+                          d=d, dim_reduction=None, cached_results=cached_results)
         ### MAKE UNCERTAIN PLOTS POSITIONSPLITTER
         plot_uncertainty_eval(datasets=datasets, reps=reps,
                           algos=algos, train_test_splitter=PositionSplitter(datasets[0]), 
                           augmentations = augmentations, number_quantiles=number_quantiles, optimize=True, 
-                          d=d, dim_reduction=None)
+                          d=d, dim_reduction=None, cached_results=cached_results)
         # ### MAKE UNCERTAIN PLOTS ACROSS DIMENSIONS
         # plot_uncertainty_eval_across_dimensions(datasets=datasets, reps=reps,
         #                   algos=algos, train_test_splitter=RandomSplitter(datasets[0]),
