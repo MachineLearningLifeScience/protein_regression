828600a8043adfa89087f27444bfdadf7922644f
Richard Michael <r.michael@posteo.net>
2023-06-07 15:55:32 +0200

    fix: display dimension

--- run_single_regression_task.py
+++ run_single_regression_task.py
@@ -144,7 +144,7 @@ def run_single_regression_task(dataset: str, representation: str, method_key: st
                                 augmentation: str, dim: int=None, dim_reduction: str=NON_LINEAR, threshold: float=None, 
                                 mock: bool=False, parallel: bool=False):
     method = ALGORITHM_REGISTRY[method_key](representation, get_alphabet(dataset))
-    print(f"{dataset}: {representation} - {method_key} | {protocol.get_name()} , dim: full")
+    print(f"{dataset}: {representation} - {method_key} | {protocol.get_name()} , dim: {dim}")
     missed_assay_indices: np.ndarray = None
     # load X for CV splitting
     X, Y = load_dataset(dataset, representation=representation, augmentation=augmentation)
