af8f3d8a60629ef1759af47d99877edf6790b9ce
Richard Michael <r.michael@posteo.net>
2023-06-11 18:22:10 +0200

    added argparsing to optimization script
    for SLURM batching

--- run_optimization.py
+++ run_optimization.py
@@ -1,3 +1,4 @@
+import argparse
 from algorithm_factories import GPLinearFactory, get_key_for_factory, ALGORITHM_REGISTRY
 from algorithm_factories import GPMaternFactory, GPSEFactory, UncertainRFFactory
 import data
@@ -11,31 +12,36 @@ RUN_ON_CLUSTER = False
 datasets = ["UBQT", "CALM", "1FQG"] # "UBQT", "CALM", "1FQG"
 representations = [TRANSFORMER, ONE_HOT, ESM, EVE] # TRANSFORMER, VAE_RAND, VAE_AUX, ONE_HOT, ESM, EVE
 seeds = [11, 42, 123, 54, 2345, 987, 6538, 78543, 3465, 43245] # 11, 42, 123, 54, 2345, 987, 6538, 78543, 3465, 43245
-# seeds = [43245]
 max_iterations = 500
-run_reference_scoring = True
 
 method_factory_keys = ALGORITHM_REGISTRY.keys()
 method_factory_keys = [get_key_for_factory(f) for f in [GPSEFactory, GPMaternFactory, UncertainRFFactory, GPLinearFactory]] # GPSEFactory, GPMaternFactory, UncertainRFFactory, GPLinearFactory
-commands = []
-command_template = "python run_single_optimization_task.py -d %s -s %i -r %s -m %s"
 
-def optimization_experiment(datasets: List[str], seeds: List[int]):
+def optimization_experiment(datasets: List[str], method_keys: List[str], 
+                        representations: List[str], seeds: List[int], 
+                        budget: int=max_iterations, reference_scoring: bool=True) -> None:
     for dataset in datasets:
-        if run_reference_scoring:
+        if reference_scoring:
             run_ranked_reference_task(dataset, max_iterations=max_iterations, reference_task=EVE_DENSITY)
         for seed in seeds:
             run_ranked_reference_task(dataset, max_iterations=max_iterations, reference_task=AT_RANDOM, seed=seed)
             for representation in representations:
-                for method in method_factory_keys:
-                    if RUN_ON_CLUSTER:
-                        commands.append(command_template % (dataset, seed, representation, method))
-                    else:
-                        run_single_optimization_task(dataset, method, seed, representation, max_iterations=max_iterations, log_interval=1)
-    # print(commands[0])
-    if RUN_ON_CLUSTER:
-        execute_job_array_on_slurm_cluster(commands, cpus=8)
+                for method in method_keys:
+                    run_single_optimization_task(dataset, method, seed, representation, max_iterations=budget, log_interval=1)
 
 
 if __name__ == "__main__":
-    optimization_experiment(datasets, seeds)
+    parser = argparse.ArgumentParser(description="Experiment Specifications")
+    parser.add_argument("-d", "--data", type=str, default=datasets, choices=datasets, help="Dataset identifier.")
+    parser.add_argument("-r", "--representation", type=str, default=representations, choices=representations, help="Representation of data identifier.")
+    parser.add_argument("-m", "--method_key", type=str, default=method_factory_keys, choices=method_factory_keys, help="Method identifier.")
+    parser.add_argument("-s", "--seeds", type=int, default=seeds, help="Random seed.")
+    parser.add_argument("-b", "--budget", type=int, default=max_iterations, help="Number of optimization iterations.")
+    args = parser.parse_args()
+    datasets = [args.data] if not isinstance(args.data, list) else args.data
+    methods = [args.method_key] if not isinstance(args.method_key, list) else args.method_key
+    representations = [args.representation] if not isinstance(args.representation, list) else args.representation
+    seeds = [args.seeds] if not isinstance(args.seeds, list) else args.seeds
+
+    optimization_experiment(datasets=datasets, method_keys=methods, representations=representations, 
+                budget=args.budget, seeds=seeds)
