e0bbb4615337ed68a66554ad28b38ec8fd00144d
Richard Michael <r.michael@posteo.net>
2023-10-09 15:01:33 +0200

    update parameterized completeness tests

--- test/test_results/test_completeness.py
+++ test/test_results/test_completeness.py
@@ -50,47 +50,47 @@ result_set_opt = list(product(test_datasets[:2], test_representations[:-1], test
 result_set_toxi = list(product(["TOXI"], [ESM], [GPonRealSpace(kernel_factory= lambda: Matern52()).get_name()], test_mutation_protocols, [SPEARMAN_RHO], test_dims))
 
 
-# @pytest.mark.parametrize("dataset,representation,method,protocol_factory,metric,dim", list(result_set_one + result_set_two + result_set_tasks + result_set_toxi))
-# def test_main_results_exist(dataset, representation, method, protocol_factory, metric, dim):
-#     """
-#     Tested data-sets cover Fig.2, 3, parts of 5, Fig.6
-#     """
-#     protocols = protocol_factory(dataset) if type(protocol_factory) != list else protocol_factory
-#     for protocol in protocols:
-#         cached_filename = f"/Users/rcml/protein_regression/results/cache/results_comparison_d={dataset}_a={method}_r={representation}_m={metric}_s={protocol.get_name()[:5]}_d={dim}.pkl"
-#         if ("pos" in protocol.get_name().lower() and dataset == "TOXI") or ("fract" in protocol.get_name().lower() and dataset == "TOXI"):
-#             continue
-#         if exists(cached_filename):
-#             with open(cached_filename, "rb") as infile:
-#                 results = pickle.load(infile)
-#         else:
-#             try:
-#                 results = load_results_dict_from_mlflow(datasets=[dataset], algos=[method], metrics=[metric], reps=[representation], train_test_splitter=[protocol], cache=True, cache_fname=cached_filename)
-#             except AttributeError as _e:
-#                 assert False
-#         # account for naming ideosynch
-#         protocol_name = "Fractional" if "fract" in protocol.get_name().lower() else protocol.get_name()
-#         method = "GPsqexp" if "squared_exponential" in method else method
-#         #print(f"{protocol.get_name()} ; {dataset} ; {method} ; {representation} ; {metric}")
-#         metric_result = results.get(protocol_name).get(dataset).get(method).get(representation).get(None).get(metric)
-#         assert metric_result is not None
-#         assert len(np.array(metric_result).flatten()) > 1 # more than one result per split
+@pytest.mark.parametrize("dataset,representation,method,protocol_factory,metric,dim", list(result_set_one + result_set_two + result_set_tasks + result_set_toxi))
+def test_main_results_exist(dataset, representation, method, protocol_factory, metric, dim):
+    """
+    Tested data-sets cover Fig.2, 3, parts of 5, Fig.6
+    """
+    protocols = protocol_factory(dataset) if type(protocol_factory) != list else protocol_factory
+    for protocol in protocols:
+        cached_filename = f"/Users/rcml/protein_regression/results/cache/results_comparison_d={dataset}_a={method}_r={representation}_m={metric}_s={protocol.get_name()[:5]}_d={dim}.pkl"
+        if ("pos" in protocol.get_name().lower() and dataset == "TOXI") or ("fract" in protocol.get_name().lower() and dataset == "TOXI"):
+            continue
+        if exists(cached_filename):
+            with open(cached_filename, "rb") as infile:
+                results = pickle.load(infile)
+        else:
+            try:
+                results = load_results_dict_from_mlflow(datasets=[dataset], algos=[method], metrics=[metric], reps=[representation], train_test_splitter=[protocol], cache=True, cache_fname=cached_filename)
+            except AttributeError as _e:
+                assert False
+        # account for naming ideosynch
+        protocol_name = "Fractional" if "fract" in protocol.get_name().lower() else protocol.get_name()
+        method = "GPsqexp" if "squared_exponential" in method else method
+        #print(f"{protocol.get_name()} ; {dataset} ; {method} ; {representation} ; {metric}")
+        metric_result = results.get(protocol_name).get(dataset).get(method).get(representation).get(None).get(metric)
+        assert metric_result is not None
+        assert len(np.array(metric_result).flatten()) > 1 # more than one result per split
 
 
-# @pytest.mark.parametrize("dataset,representation,method,seed", list(result_set_opt))
-# def test_optimization_results_exist(dataset, representation, method, seed):
-#         cache_filename = f"/Users/rcml/protein_regression/results/cache/results_optimization_d={dataset}_a={method}_r={representation}_s={seed}.pkl"
-#         # ALL ALGO RESULTS:
-#         if exists(cache_filename):
-#             with open(cache_filename, "rb") as infile:
-#                 results = pickle.load(infile)
-#         else:
-#             results = get_mlflow_results_optimization(datasets=[dataset], algos=[method], reps=[representation], metrics=[OBSERVED_Y], seeds=[seed])
-#             with open(cache_filename, "wb") as outfile:
-#                 pickle.dump(results, outfile)
-#         method = "GPsqexp" if "squared_exponential" in method else method
-#         assert results[seed][dataset][method][representation][None][OBSERVED_Y] is not None
-#         assert len(np.array(results[seed][dataset][method][representation][None][OBSERVED_Y]).flatten()) > 1
+@pytest.mark.parametrize("dataset,representation,method,seed", list(result_set_opt))
+def test_optimization_results_exist(dataset, representation, method, seed):
+        cache_filename = f"/Users/rcml/protein_regression/results/cache/results_optimization_d={dataset}_a={method}_r={representation}_s={seed}.pkl"
+        # ALL ALGO RESULTS:
+        if exists(cache_filename):
+            with open(cache_filename, "rb") as infile:
+                results = pickle.load(infile)
+        else:
+            results = get_mlflow_results_optimization(datasets=[dataset], algos=[method], reps=[representation], metrics=[OBSERVED_Y], seeds=[seed])
+            with open(cache_filename, "wb") as outfile:
+                pickle.dump(results, outfile)
+        method = "GPsqexp" if "squared_exponential" in method else method
+        assert results[seed][dataset][method][representation][None][OBSERVED_Y] is not None
+        assert len(np.array(results[seed][dataset][method][representation][None][OBSERVED_Y]).flatten()) > 1
 
 
 ### TESTING ABLATION MATERIAL COMPLETENESS:
@@ -124,7 +124,7 @@ test_set_ablation_embeddings_regressors = list(product(
     [LINEAR],
 ))
 
-ablation_test_iterator = test_set_ablation_mutation #+ test_set_ablation_dims + test_set_ablation_embeddings_regressors
+ablation_test_iterator = test_set_ablation_mutation + test_set_ablation_dims + test_set_ablation_embeddings_regressors
 
 @pytest.mark.parametrize("dataset,representation,method,protocol,metric,dim,dim_reduction", list(ablation_test_iterator))
 def test_ablation_results_exist(dataset, representation, method, protocol, metric, dim, dim_reduction):
