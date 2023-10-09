8d2ce4c239dd720c522f7183a56158f690c8d41c
Richard Michael <r.michael@posteo.net>
2023-10-09 15:02:18 +0200

    update splitting SLURM config

--- slurm_configs/optimal_parameter_splits.sh
+++ slurm_configs/optimal_parameter_splits.sh
@@ -1,8 +1,8 @@
 # for one_hot, esm, transformer representation
 
-OPTIMAL_RF_PARAM_FILENAME_REFERENCE="RF_optimal_estimators_1FQG_transformer_RandomSplitter_None_s"
-OPTIMAL_RF_PARAM_FILENAME="RF_optimal_estimators_1FQG_transformer_FractionalRandomSplitter"
-FRACTIONS=("0.45" "0.51" "0.54" "0.57" "0.6" "0.65" "0.7" "0.75" "0.8" "0.85" "0.85" "0.9" "0.95" "1.0")
+OPTIMAL_RF_PARAM_FILENAME_REFERENCE="RF_optimal_estimators_1FQG_esm_RandomSplitter_None_s"
+OPTIMAL_RF_PARAM_FILENAME="RF_optimal_estimators_1FQG_esm_FractionalRandomSplitter"
+FRACTIONS=("0.36" "0.39" "0.42" "0.45" "0.48" "0.51" "0.54" "0.57" "0.6" "0.65" "0.7" "0.75" "0.8" "0.85" "0.85" "0.9" "0.95" "1.0")
 
 for s in ${FRACTIONS[@]}; do
     for split in $(seq 1 4); do
